190cc5ed0eef07a4de504e0b778205315cd0d93e
thiyangt <tstalagala@gmail.com>
2024-07-02 12:36:18 +0530

    updated

---
+++ code/tickets_us.R
@@ -0,0 +1,6 @@
+#Tickets issued for parking violations in the city of Philadelphia, 
+#Pennsylvania in 2017
+
+library(tidyverse)
+
+philly <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-12-03/tickets.csv")
